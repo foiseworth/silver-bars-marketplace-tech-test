91b1b0466db35ea3fc48cbdb107cfab43d5fc06d
Andrew Herrington <a.d.herrington@gmail.com>
2017-02-26 08:09:44 +0000

    Add startup command and logging of requests

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = require('express')();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  console.log(`${req.method} to ${req.path}`); // eslint-disable-line no-console
+  next();
+});
+
 app.post('/register', require('./api/register'));
 
 // app.post('/cancel', require('./api/cancel'));
@@ -12,5 +17,5 @@ app.post('/register', require('./api/register'));
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Application available at http://localhost:${port}`); // eslint-disable-line no-console
-  console.log('Available routes are: register|cancel|summary'); // eslint-disable-line no-console
+  console.log('Available routes are: register[POST]|cancel[POST]|summary[GET]'); // eslint-disable-line no-console
 });
